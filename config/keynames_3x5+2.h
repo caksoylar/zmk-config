44cbfd372cfd30dc7a09a27789ed1022a2d6a9d9
moutis <35399827+moutis@users.noreply.github.com>
2025-07-22 07:35:27 -0400

    Complete abstraction for Zaphod

--- config/keynames_3x5+2.h
+++ config/keynames_3x5+2.h
@@ -5,21 +5,21 @@
 //  Created by Alan on 7/24/22.
 //
 
-/*    KEY POSITION Numbers for BandWings (3x5+3)
-  ╭───────────────────────╮ ╭────────────────────────╮
-  │  0    1   2    3    4 │ │  5    6    7    8    9 │
-  │ 10   11  12   13   14 │ │ 15   16   17   18   19 |
-  | 20   21  22   23   24 │ │ 25   26   27   28   29 |
-  ╰───────╮  30   31   32 | | 32   33   34 ╭─────────╯
-          ╰───────────────╯ ╰──────────────╯
+/*    KEY POSITION Numbers for 3x5+2
+╭────────────────────╮  ╭────────────────────╮
+│  0   1   2   3   4 │  │  5   6   7   8   9 │
+│ 10  11  12  13  14 │  │ 15  16  17  18  19 |
+| 20  21  22  23  24 │  │ 25  26  27  28  29 |
+╰───────────╮ 30  31 |  | 33  33 ╭───────────╯
+            ╰────────╯  ╰────────╯
 */
 /*    KEY POSITION Names
-    ╭─────────────────────╮ ╭─────────────────────╮
-LT5 │ LT4 LT3 LT2 LT1 LT0 │ │ RT0 RT1 RT2 RT3 RT4 │ RT5
-LM5 │ LM4 LM3 LM2 LM1 LM0 │ │ RM0 RM1 RM2 RM3 RM4 │ RM5
-LB5 │ LB4 LB3 LB2 LB1 LB0 │ │ RB0 RB1 RB2 RB3 RB4 │ RB5
-    ╰───────╮ LH3 LH2 LH1 | | RH1 RH2 RH3 ╭───────╯
-            ╰─────────────╯ ╰─────────────╯
+╭─────────────────────╮ ╭─────────────────────╮
+│ LT4 LT3 LT2 LT1 LT0 │ │ RT0 RT1 RT2 RT3 RT4 │
+│ LM4 LM3 LM2 LM1 LM0 │ │ RM0 RM1 RM2 RM3 RM4 │
+│ LB4 LB3 LB2 LB1 LB0 │ │ RB0 RB1 RB2 RB3 RB4 │
+╰───────╮     LH2 LH1 | | RH1 RH2     ╭───────╯
+        ╰─────────────╯ ╰─────────────╯
 */
 
 // define the key names for this board
@@ -56,13 +56,11 @@ LB5 │ LB4 LB3 LB2 LB1 LB0 │ │ RB0 RB1 RB2 RB3 RB4 │ RB5
 #define RB3 28
 #define RB4 29
 
-#define LH3 30
-#define LH1 32
-#define LH2 31
-#define RH1 33
-#define RH2 34
-#define RH3 35
+#define LH1 31
+#define LH2 30
+#define RH1 32
+#define RH2 33
 
 #define KEYS_L LT0 LT1 LT2 LT3 LT4 LM0 LM1 LM2 LM3 LM4 LB0 LB1 LB2 LB3 LB4  // left-hand keys
 #define KEYS_R RT0 RT1 RT2 RT3 RT4 RM0 RM1 RM2 RM3 RM4 RB0 RB1 RB2 RB3 RB4  // right-hand keys
-#define THUMBS LH3 LH2 LH1 RH1 RH2 RH3                                      // thumb keys
+#define THUMBS LH2 LH1 RH1 RH2                                              // thumb keys
