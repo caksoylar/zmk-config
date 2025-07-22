1baf2becde318f287b81bb40f7d58d8d370c999a
moutis <35399827+moutis@users.noreply.github.com>
2025-07-22 14:37:06 -0400

    Update zaphod_status_screen.c

--- config/boards/arm/zaphod/zaphod_status_screen.c
+++ config/boards/arm/zaphod/zaphod_status_screen.c
@@ -93,7 +93,7 @@ lv_obj_t *zmk_display_status_screen() {
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_WPM_STATUS)
     zmk_widget_wpm_status_init(&wpm_status_widget, screen);
     lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), LV_ALIGN_BOTTOM_RIGHT, 0, 0);
-    lv_obj_set_y(zmk_widget_wpm_status_obj(&wpm_status_widget), lv_obj_get_y(zmk_widget_wpm_status_obj(&wpm_status_widget)-24);
+    lv_obj_set_y(zmk_widget_wpm_status_obj(&wpm_status_widget), lv_obj_get_y(zmk_widget_wpm_status_obj(&wpm_status_widget))-24);
 #endif
     return screen;
 }
